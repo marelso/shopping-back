b96de409bbeb31d7c916fbbdc961cfdb29fdb897
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-02-27 14:43:00 -0300

    Include offer to category relation representation entity.

--- src/main/java/com/example/shopping/domain/OfferCategory.java
+++ src/main/java/com/example/shopping/domain/OfferCategory.java
@@ -1,2 +1,20 @@
-package com.example.shopping.domain;public class OfferCategory {
+package com.example.shopping.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name="offers_categories")
+@Getter
+@Setter
+public class OfferCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer categoryId;
+    private Integer offersId;
 }
