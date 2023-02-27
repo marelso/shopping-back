ee800e0ba9a5dba63a5af88d1c771c4f90c78a52
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-02-27 04:52:10 -0300

    Finished notfound exception test

--- src/main/java/com/example/shopping/service/CouponService.java
+++ src/main/java/com/example/shopping/service/CouponService.java
@@ -1,8 +1,21 @@
 package com.example.shopping.service;
 
+import com.example.shopping.domain.Coupon;
+import com.example.shopping.exception.NotFoundException;
+import com.example.shopping.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CouponService {
+    private final CouponRepository repository;
+    public Coupon findById(Integer id) {
+        return get(id);
+    }
 
+    private Coupon get(Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Coupon", id));
+    }
 }
--- src/test/java/com/example/shopping/service/CouponServiceTest.java
+++ src/test/java/com/example/shopping/service/CouponServiceTest.java
@@ -2,6 +2,7 @@ package com.example.shopping.service;
 
 import static com.googlecode.catchexception.CatchException.catchException;
 import com.example.shopping.domain.Coupon;
+import com.example.shopping.exception.NotFoundException;
 import com.example.shopping.repository.CouponRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +12,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -24,12 +29,16 @@ class CouponServiceTest {
 
     @Test
     public void shouldThrowExceptionWhenThereIsNoCouponWithGivenId() {
-        final Integer couponId = Integer.valueOf(1);
+        final Integer couponId = 1;
         final Optional<Coupon> coupon = Optional.empty();
 
         given(repository.findById(couponId)).willReturn(coupon);
 
 
-        catchException(() -> subject.get(couponId));
+        catchException(() -> subject.findById(couponId));
+
+
+        assertThat(caughtException(), instanceOf(NotFoundException.class));
+        then(repository).should().findById(couponId);
     }
 }
