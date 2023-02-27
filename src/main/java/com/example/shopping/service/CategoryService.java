d9f92d0c7e3980e19d2644c32f248ac4a2cd5897
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-02-27 08:10:23 -0300

    Include find by id method in category service.

--- src/main/java/com/example/shopping/service/CategoryService.java
+++ src/main/java/com/example/shopping/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.example.shopping.service;
 
+import com.example.shopping.domain.Category;
+import com.example.shopping.exception.NotFoundException;
 import com.example.shopping.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository repository;
+
+    public Category findById(Integer id) {
+        return get(id);
+    }
+
+    private Category get(Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Category", id));
+    }
 }
