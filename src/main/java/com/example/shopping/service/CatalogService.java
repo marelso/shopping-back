4eb4647f1aad653a12fcfb269170b936006b8f34
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-02-27 06:28:16 -0300

    Include find method in catalog service.

--- src/main/java/com/example/shopping/service/CatalogService.java
+++ src/main/java/com/example/shopping/service/CatalogService.java
@@ -1,5 +1,7 @@
 package com.example.shopping.service;
 
+import com.example.shopping.domain.Catalog;
+import com.example.shopping.exception.NotFoundException;
 import com.example.shopping.repository.CatalogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CatalogService {
     private final CatalogRepository repository;
+
+    public Catalog findById(Integer id) {
+        return get(id);
+    }
+
+    private Catalog get(Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Catalog", id));
+    }
 }
