302e0f5a7f12e18d2d995587d417a17be5513749
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-02-27 05:57:58 -0300

    Include rest operations to new endpoint that will manage coupon requests.

--- src/main/java/com/example/shopping/controller/CouponController.java
+++ src/main/java/com/example/shopping/controller/CouponController.java
@@ -1,11 +1,42 @@
 package com.example.shopping.controller;
 
+import com.example.shopping.domain.Coupon;
+import com.example.shopping.service.CouponService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/coupons")
 @RequiredArgsConstructor
 public class CouponController {
+    private final CouponService service;
+
+    @GetMapping
+    public List<Coupon> get() {
+        return service.list();
+    }
+
+    @GetMapping("/{id}")
+    public Coupon get(@PathVariable Integer id) {
+        return service.findById(id);
+    }
+
+    @PostMapping
+    public Coupon post(@RequestBody Coupon request) {
+        return service.create(request);
+    }
+
+    @PutMapping("/{id}")
+    public Coupon put(
+            @PathVariable Integer id,
+            @RequestBody Coupon request) {
+        return service.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        service.delete(id);
+    }
 }
