2461f7edfac9d040d226f72d998b958f60b18314
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-02-27 06:48:42 -0300

    Including update method in catalog service.

--- src/main/java/com/example/shopping/service/CatalogService.java
+++ src/main/java/com/example/shopping/service/CatalogService.java
@@ -2,6 +2,7 @@ package com.example.shopping.service;
 
 import com.example.shopping.domain.Catalog;
 import com.example.shopping.exception.NotFoundException;
+import com.example.shopping.factory.CatalogFactory;
 import com.example.shopping.repository.CatalogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CatalogService {
     private final CatalogRepository repository;
+    private final CatalogFactory factory;
 
     public Catalog findById(Integer id) {
         return get(id);
@@ -28,5 +30,11 @@ public class CatalogService {
 
     public List<Catalog> list() {
         return repository.findAll();
+    }
+
+    public Catalog update(Integer id, Catalog request) {
+        var catalog = get(id);
+
+        return repository.save(factory.from(catalog, request));
     }
 }
