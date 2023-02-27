9efa63602a21acb1ba1ee09b62f41fb2cfd88fc4
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-02-27 04:44:35 -0300

    Include new dependency to help test development

--- src/test/java/com/example/shopping/service/CouponServiceTest.java
+++ src/test/java/com/example/shopping/service/CouponServiceTest.java
@@ -1,7 +1,35 @@
 package com.example.shopping.service;
 
+import static com.googlecode.catchexception.CatchException.catchException;
+import com.example.shopping.domain.Coupon;
+import com.example.shopping.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
+
+@ExtendWith(MockitoExtension.class)
 class CouponServiceTest {
+    @Mock
+    private CouponRepository repository;
+    @InjectMocks
+    private CouponService subject;
 
+    @Test
+    public void shouldThrowExceptionWhenThereIsNoCouponWithGivenId() {
+        final Integer couponId = Integer.valueOf(1);
+        final Optional<Coupon> coupon = Optional.empty();
+
+        given(repository.findById(couponId)).willReturn(coupon);
+
+
+        catchException(() -> subject.get(couponId));
+    }
 }
