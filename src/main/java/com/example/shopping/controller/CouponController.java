23a51a9a53b70043018b06a86483a62026067614
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-02-27 05:41:24 -0300

    Include new controller to handle coupon requests.

---
+++ src/main/java/com/example/shopping/controller/CouponController.java
@@ -0,0 +1,11 @@
+package com.example.shopping.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coupons")
+@RequiredArgsConstructor
+public class CouponController {
+}
