b1e703f631e60d56d9cd208f050559bbf59e6163
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-02-28 01:49:16 -0300

    Creating offer category relation service methods.

--- src/main/java/com/example/shopping/service/OfferCategoryService.java
+++ src/main/java/com/example/shopping/service/OfferCategoryService.java
@@ -1,11 +1,42 @@
 package com.example.shopping.service;
 
+import com.example.shopping.domain.OfferCategory;
+import com.example.shopping.exception.NotFoundException;
 import com.example.shopping.repository.OfferCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OfferCategoryService {
     private final OfferCategoryRepository repository;
+
+    public List<OfferCategory> findAll() {
+        return repository.findAll();
+    }
+
+    public OfferCategory findById(Integer id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Offer category relation", id));
+    }
+
+    public List<OfferCategory> findAllByOffersId(Integer id) {
+        return repository.findAllByOffersId(id);
+    }
+
+    public OfferCategory save(OfferCategory offersCategories) {
+        return repository.save(offersCategories);
+    }
+
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
+
+    @Transactional
+    public void deleteByOffersId(Integer id) {
+        repository.deleteByOffersId(id);
+    }
 }
